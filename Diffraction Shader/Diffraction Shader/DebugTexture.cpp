529b8f8fc9b589403f9982f2da93fbae003d8a9d
Elliott Zwertvaegher <elliott.zwertvaegher@epitech.eu>
2017-06-14 21:37:23 +0900

    yolo j'me souviens plus

---
+++ Diffraction Shader/Diffraction Shader/DebugTexture.cpp
@@ -0,0 +1,20 @@
+#include "DebugTexture.h"
+
+
+
+DebugTexture::DebugTexture()
+{
+}
+
+
+DebugTexture::~DebugTexture()
+{
+}
+
+void DebugTexture::Draw(glm::mat4 & model, glm::mat4 & view, glm::mat4 & projection)
+{
+}
+
+void DebugTexture::DrawDepth(glm::mat4 & model, glm::mat4 & view, glm::mat4 & projection, IShader * depthShader)
+{
+}
---
+++ Diffraction Shader/Diffraction Shader/DebugTexture.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#ifndef _DEBUG_TEXTURE_H_
+#define _DEBUG_TEXTURE_H_
+
+#include "IDrawable.h"
+
+class DebugTexture : public IDrawable
+{
+public:
+	DebugTexture();
+	~DebugTexture();
+};
+
+#endif // !_DEBUG_TEXTURE_H_
