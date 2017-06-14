bb5503ba2e61f73dd1f2cf4d16ec69018e7a9a43
Elliott Zwertvaegher <elliott.zwertvaegher@epitech.eu>
2017-06-14 21:39:32 +0900

    yolo2

--- Diffraction Shader/Diffraction Shader/DebugTexture.h
+++ Diffraction Shader/Diffraction Shader/DebugTexture.h
@@ -10,6 +10,10 @@ class DebugTexture : public IDrawable
 public:
 	DebugTexture();
 	~DebugTexture();
+
+	// Hérité via IDrawable
+	virtual void Draw(glm::mat4 & model, glm::mat4 & view, glm::mat4 & projection) override;
+	virtual void DrawDepth(glm::mat4 & model, glm::mat4 & view, glm::mat4 & projection, IShader * depthShader) override;
 };
 
 #endif // !_DEBUG_TEXTURE_H_
